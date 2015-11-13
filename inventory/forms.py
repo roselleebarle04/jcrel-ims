28fa4663321ee992be08aa3b9191f13ca4ba6d60
erikkaBaguio <erikka.baguio@gmail.com>
2015-11-13 08:19:50 +0800

    Updated AdAdd Sale Template

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from .models import Accounts,Transfer_item,AddArrival, Item
+from .models import Accounts,Transfer_item,AddArrival, Item, Sale
 
 class AccountsForm(forms.ModelForm):
 	class Meta:
 		widgets = {
 		'password': forms.PasswordInput()
 		}
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['store_code', 'unit_cost', 'category','brand','item_model','supplier', 'supplier_code']
-		
 
 class ArrivalForm(forms.ModelForm):
     class Meta:
