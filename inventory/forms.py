544e5045c58a6260561eb73ed91147487214d114
erikkaBaguio <erikka.baguio@gmail.com>
2015-11-17 07:35:46 +0800

    Added Supplier Form

--- inventory/forms.py
+++ inventory/forms.py
@@ -2,7 +2,7 @@
 
 
 from django import forms
-from .models import Account,Transfer_item,AddArrival, Item, Sale
+from .models import Account,Transfer_item,AddArrival, Item, Sale, Supplier
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -50,6 +50,11 @@ class AddSaleForm(forms.ModelForm):
     class Meta:
         model = Sale
         fields = ['item', 'quantity', 'date']
+
+class AddSupplierForm(forms.ModelForm):
+	class Meta:
+        model = Sale
+        fields = '__all__'
 
 class ArrivalForm(forms.ModelForm):
     class Meta:
