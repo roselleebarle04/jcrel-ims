698745c4825e9b1c63836bfd944de02147893f2b
JosiahRegencia <jawshaeleazar@gmail.com>
2015-12-01 01:30:00 +0800

    edited base to have a facebook-like notification ui, though still has bugs

---
+++ inventory/static/js/base.js
@@ -0,0 +1,25 @@
+<script type="text/javascript" src="static/js/jquery.min.1.11.js"></script>
+<script type="text/javascript" >
+$(document).ready(function()
+{
+$("#notificationLink").click(function()
+{
+$("#notificationContainer").fadeToggle(300);
+$("#notification_count").fadeOut("slow");
+return false;
+});
+
+//Document Click hiding the popup 
+$(document).click(function()
+{
+$("#notificationContainer").hide();
+});
+
+//Popup on click
+$("#notificationContainer").click(function()
+{
+return false;
+});
+
+});
+</script>
